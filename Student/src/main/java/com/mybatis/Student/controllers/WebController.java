ee75f113fd46e52d683204ed6322f3c8b47209fb
TTNthinkpad <ajay.kumar1@tothenew.com>
2020-12-15 15:57:16 +0530

    User Page is added

--- Student/src/main/java/com/mybatis/Student/controllers/WebController.java
+++ Student/src/main/java/com/mybatis/Student/controllers/WebController.java
@@ -1,18 +1,30 @@
 package com.mybatis.Student.controllers;
 
+import com.mybatis.Student.entities.User;
+import com.mybatis.Student.mappers.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 public class WebController {
 
+    @Autowired
+    UserMapper userMapper;
+
     @RequestMapping(value = "/")
-    public String index() {
-        return "index";
+    public String index(Principal principal) {
+        User user = userMapper.findByUserName(principal.getName());
+
+        if (user.getRole().equalsIgnoreCase("Admin"))
+            return "index";
+        else return "indexUser";
     }
 
     @RequestMapping(value = "/login")
-    public String login(){
+    public String login() {
         return "login";
     }
 
