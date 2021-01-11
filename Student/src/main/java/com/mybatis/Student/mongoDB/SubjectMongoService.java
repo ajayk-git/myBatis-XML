e3c7c4f8ea8ca033df0243478ddbe8771982e619
TTNthinkpad <ajay.kumar1@tothenew.com>
2021-01-11 16:51:00 +0530

    Docker implemented

--- Student/src/main/java/com/mybatis/Student/mongoDB/SubjectMongoService.java
+++ Student/src/main/java/com/mybatis/Student/mongoDB/SubjectMongoService.java
@@ -1,32 +1,13 @@
 package com.mybatis.Student.mongoDB;
-
 import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import com.mongodb.client.AggregateIterable;
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mybatis.Student.exceptions.ResourceNotFound;
-import org.bson.BsonNull;
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Arrays;
-
-import org.bson.BsonNull;
-
-import static com.mongodb.client.model.Accumulators.addToSet;
-import static com.mongodb.client.model.Aggregates.*;
-import static com.mongodb.client.model.Projections.excludeId;
-import static com.mongodb.client.model.Projections.fields;
-import static com.mongodb.client.model.Projections.include;
-
 
 @Service
 public class SubjectMongoService {
